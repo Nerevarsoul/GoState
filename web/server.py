3e7d3a2edc62b8528e7264dca09f92dc3064a979
Neri <keshaeverywhere@gmail.com>
2016-04-18 22:09:49 +0300

    Tiny fix for gunicorn and add index.html

--- web/server.py
+++ web/server.py
@@ -6,7 +6,7 @@ from werkzeug.contrib.fixers import ProxyFix
 basedir = os.path.join(os.path.abspath(os.path.dirname(__file__)), '../')
 
 app = Flask(__name__)
-app.config.from_object('app.config')
+app.config.from_object('web.config')
 app.config['SESSION_TYPE'] = 'memcached'
 app.config['SECRET_KEY'] = 'super secret key'
 
