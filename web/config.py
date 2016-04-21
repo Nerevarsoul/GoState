9fd20d49d502c5e5ad226295819257f45b06fa04
Neri <keshaeverywhere@gmail.com>
2016-04-21 22:31:56 +0300

    Add database settings.

--- web/config.py
+++ web/config.py
@@ -1,1 +1,18 @@
-DEBUG = True
+import os
+
+
+class Config(object):
+
+    DEBUG = True
+
+    # config db
+    DB_USER_NAME = os.environ['DB_USER_NAME']
+    DB_PASSWORD = os.environ['DB_PASSWORD']
+    DB_HOST = "localhost"
+    DB_BASE_NAME = os.environ['DB_BASE_NAME']
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
--- web/server.py
+++ web/server.py
@@ -1,14 +1,19 @@
 import os
 
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 from werkzeug.contrib.fixers import ProxyFix
 
 basedir = os.path.join(os.path.abspath(os.path.dirname(__file__)), '../')
 
 app = Flask(__name__)
-app.config.from_object('web.config')
+app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SESSION_TYPE'] = 'memcached'
 app.config['SECRET_KEY'] = 'super secret key'
+
+
+# flask-sqlalchemy
+db = SQLAlchemy(app)
 
 import web.views
 app.wsgi_app = ProxyFix(app.wsgi_app)
