117af09bd170d3bad2a692fb0308a54ba3a3a8f3
Innokentiy Zabirov <keshaeverywhere@gmail.com>
2016-06-06 14:33:31 +0300

    Update server.py

--- web/server.py
+++ web/server.py
@@ -17,3 +17,17 @@ db = SQLAlchemy(app)
 
 import web.views
 app.wsgi_app = ProxyFix(app.wsgi_app)
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(os.environ['APP_SETTINGS'])
+
+    db.init_app(app)
+    with app.test_request_context():
+        db.create_all()
+
+    import app.firstmodule.controllers as firstmodule
+
+    app.register_blueprint(firstmodule.module)
+
+    return app
