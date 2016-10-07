15da476566246f32b8f41729ef551da966d68b7c
Innokentiy Zabirov <keshaeverywhere@gmail.com>
2016-10-08 00:19:26 +0400

    Update views.py

--- web/views.py
+++ web/views.py
@@ -1,8 +1,15 @@
-from flask import render_template
+from flask import render_template, jsonify, request
 
 from .server import app
+from .models import Title
 
 
 @app.route('/')
 def index_view():
     return render_template('index.html')
+
+
+@app.route('/titles')
+def index_view():
+    titles = Title.objects.all()
+    return jsonify(titles=titles)
