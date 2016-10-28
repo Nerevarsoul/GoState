7eff968e85223357cb4eea4c34b6f18d7257ccee
Nerevarsoul <keshaeverywhere@gmail.com>
2016-10-28 11:32:09 +0200

    add handler for 400-500 errors.

--- web/views.py
+++ web/views.py
@@ -23,3 +23,27 @@ def title_view(title_id):
     titles = Title.query.get(id=title_id)
     return jsonify(title=title)
 
+
+@app.errorhandler(400)
+def internal_error(exception):
+    app.logger.error(exception)
+    return render_template('400.html'), 400
+
+
+@app.errorhandler(403)
+def internal_error(exception):
+    app.logger.error(exception)
+    return render_template('403.html'), 403
+
+
+@app.errorhandler(404)
+def internal_error(exception):
+    app.logger.error(exception)
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_error(exception):
+    app.logger.error(exception)
+    return render_template('500.html'), 500
+
