70ef2adeb83dca56f24b420ab913244594216cab
Innokentiy Zabirov <keshaeverywhere@gmail.com>
2016-10-31 16:00:33 +0300

    Update models.py

--- web/models.py
+++ web/models.py
@@ -21,7 +21,7 @@ class Player(db.Model):
         return '<Player object {}>'.format(self.id)
 
     def __str__(self):
-        return '{}, {}'.format(self.first_name, self.last_name)
+        return '{} {}'.format(self.last_name, self.first_name)
     
     
 class Title(db.Model):
