beb4dbd152cba2e283af97f6dd2e31d6e9239291
Nerevarsoul <keshaeverywhere@gmail.com>
2016-11-25 14:42:42 +0100

    Init celery

--- celery_tasks/tasks.py
+++ celery_tasks/tasks.py
@@ -1,7 +1,11 @@
+from celery import Celery
 from celery.task.schedules import crontab
 from celery.task import task, periodic_task
 
 from .helpers import igokisen_get_json, update_title
+
+
+app = Celery('tasks', broker='pyamqp://guest@localhost//')
 
 
 @periodic_task(run_every=crontab(minute=0, hour=4))
