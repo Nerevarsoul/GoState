bd9ded8c5fbae3b22bfed15fd83294d8091afd09
Innokentiy Zabirov <keshaeverywhere@gmail.com>
2017-02-24 13:03:19 +0400

    Create tests.py

---
+++ celery_tasks/tests.py
@@ -0,0 +1,16 @@
+from unittest import TestCase
+
+from .helpers import get_game_data
+
+
+class TestGetGameData(TestCase):
+    """"""
+    
+    def setUp(self):
+        self.test_function = get_game_data
+    
+    def test_igokisen_games(self):
+        pass
+        
+    def test_gokifu_games(self):
+        pass
